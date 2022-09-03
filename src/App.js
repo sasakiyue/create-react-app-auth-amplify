66c58aaa901c067f5fff70f8aeec8a3235c5a266
Yo Sasaki <yws10lsee@gmail.com>
2022-09-04 01:01:50 +0900

    first chaange

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import Amplify from "aws-amplify";
+import aws_exports from "./aws-exports";
 Amplify.configure(aws_exports);
 
 class App extends Component {
@@ -25,6 +25,8 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <h1>sample App</h1>
+        <p>これはサンプルで作成したアプリです</p>
       </div>
     );
   }
